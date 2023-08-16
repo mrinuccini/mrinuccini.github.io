acb7450cbfb88e58f87d9e551d90bfcfb9ab516c
Matthias <66728482+mrinuccini@users.noreply.github.com>
2023-08-16 21:33:08 +0200

    Update 1 fix
    Fixed a bunch of bugs in the previous update :
    Fixed website not being correctly displayed on monitors with a huge resolution
    Fixed icons not being the correct color (play icon was black and not green)
    Changed the project section text div to be at the right of the project preview rather than on the project preview (user suggestion)
    Fixed mobile navbar not being correctly displayed

--- js/main.js
+++ js/main.js
@@ -5,3 +5,6 @@ if(screenWidth <= 1080)
 {
     alert("The mobile version of the website isn't fully complete. Please note that bugs and weird visuals will be patched.");
 }
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
